344e3cde58b064dd1908f8b469949230a1b8c0a0
Leonardo Almir <leonardostynner@live.com>
2024-07-26 23:35:49 -0300

    Sobre variáveis

--- 02-variaveis.php
+++ 02-variaveis.php
@@ -4,5 +4,27 @@ $nome = "Leonardo Almir";
 $idade = 36;
 $altura = 1.71;
 
-
 echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+
+// Alterando meus dados
+$nome = "Ronaldinho Gaucho";
+$idade = 41;
+$altura = 1.80;
+
+echo "<br>";
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+
+// Nomes válidos para variáveis
+$nome;
+$_nome;
+$nome2;
+$meu_nome;
+$meuNome;
+
+// Nomes inválidos
+// $1nome;
+// $carr@@;
+// $meu nome;
+
+// Variáveis em maiusculo e minuscula, são consideradas diferentes. (case sensitive).
+// $carro é diferente de $Carro;
