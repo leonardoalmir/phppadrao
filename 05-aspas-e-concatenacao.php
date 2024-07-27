d232c71f7a167723421e778101b94584d15f3df6
Leonardo Almir <leonardostynner@live.com>
2024-07-27 00:14:59 -0300

    Sobre aspas e concatenação

---
+++ 05-aspas-e-concatenacao.php
@@ -0,0 +1,19 @@
+<?php 
+echo "<h2>Aspas simples</h2>";
+echo "<p>Aspas simples são literais</p>";
+$nome = 'Leonardo almir';
+echo $nome;
+echo "<br>";
+echo 'Meu nome é $nome'; // aspas simples são literais
+echo "<br>";
+//Concatenação
+echo 'Meu nome é '.$nome.' e minha idade é 36';
+echo "<br>";
+//Imprimindo aspas simples dentro de uma string com aspas simples
+echo 'Meu nome é '.$nome.' e minha idade é \'36\'';
+echo "<hr>";
+echo "<h2>Aspas duplas</h2>";
+echo "<p>Aspas duplas são interpretativas</p>";
+echo "Meu nome é $nome. Entre \"aspas duplas\"";
+
+
