4fc2bb4546bec822101f9700ba7d4f6385abf423
Leonardo Almir <leonardostynner@live.com>
2024-07-26 23:23:40 -0300

    Add arquivos iniciais do projeto

---
+++ 01-fundamentos-basicos.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Padrão</title>
+</head>
+<body>
+    <?php 
+        // Comentário de uma linha
+        /* 
+        Comentário de multiplas linhas
+        pode ser escrito em várias linhas
+        */
+        echo "<h1>Bem-vindo(a)!</h1>";
+        print "<br> Olá, mundo!";
+    ?>
+</body>
+</html>
---
+++ 02-variaveis.php
@@ -0,0 +1,8 @@
+<?php 
+// Meus dados
+$nome = "Leonardo Almir";
+$idade = 36;
+$altura = 1.71;
+
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
