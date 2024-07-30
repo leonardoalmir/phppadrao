cecb51d9f34aea6ac2a269bca3221efa6998fdc9
Leonardo Almir <leonardostynner@live.com>
2024-07-30 19:26:53 -0300

    Aula sobre constantes

--- 07-constantes.php
+++ 07-constantes.php
@@ -1,2 +1,26 @@
 <?php 
-// Constantes
+// Constantes - Valor único - Só pode ser definida uma vez
+
+define("NOME", "Leonardo Almir");
+define("IDADE", 36);
+define("ALTURA", 1.71);
+define("CASADO", true);
+
+echo "Meu nome é ".NOME." e minha idade é ".IDADE.".";
+
+define("TIMES", ['Cruzeiro', 'Atletico', 'America MG']);
+
+echo "<hr>";
+
+var_dump(TIMES);
+
+echo "O melhor de Minas é o ".TIMES[0].".";
+
+echo "<hr>";
+/* Contantes tem escopo global */
+function exibeNome() {
+    echo NOME;
+}
+
+exibeNome();
+
